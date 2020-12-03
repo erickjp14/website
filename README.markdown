6b9b11c2bea61c5b89f8f00c0e1ec42db368b528
erickjp14 <75455175+erickjp14@users.noreply.github.com>
2020-12-03 17:21:41 -0600

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# HOLA XD
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/erick-jarquin-pacheco/pen/vYXLZZp](https://codepen.io/erick-jarquin-pacheco/pen/vYXLZZp).
+
+
---
+++ src/script.js
@@ -0,0 +1,14 @@
+let si=document.getElementById('si');
+si.addEventListener("click",function(e){
+  alert("Que bieeeeen🥳, te quiero  🧡 ");
+  
+} );
+
+let no=document.getElementById('no');
+no.addEventListener("mousemove",function(e){
+ let x=Math.round(Math.random() * 93);
+ let y=Math.round(Math.random() * 93);
+  no.style.left=x+"%";
+  no.style.top=y+"%";
+  
+} );
